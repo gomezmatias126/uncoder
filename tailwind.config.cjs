cf6f2499789e5ababfca883e0a8c961c8e716421
Matias Gomez <gomezmatias126@gmail.com>
2024-04-18 08:23:24 -0300

    arreglados los textos

---
+++ tailwind.config.cjs
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+let defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container:{
+			center:true,
+			padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '4rem',
+      },
+    },
+		fontFamily:{
+			'sans': ['"DM Sans"', ...defaultTheme.fontFamily.sans]
+		},
+		fontWeight:{
+			regular:'400',
+			medium: '500',
+			bold: '700'
+		},
+		extend: {
+			colors:{
+				'verde':'#3AC989',
+				'celeste':'#8CD6F7',
+				'violeta':'#883BFF',
+				'naranja':'#FAB82C',
+				'light':'#FCFEFF',
+				'grey':{
+					DEFAULT:'#808080',
+					text:'#BBBBBB',
+					light:'#DDDDDD'
+				},
+				'dark': {
+					DEFAULT:'#090A05',
+					light: '#2B2C27',
+					tenue: '#3E4036'
+				}
+			},
+		},
+	},
+	plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        '.btn': {
+					display: 'flex',
+					padding: '12px 40px',
+					gap: '16px',
+					background: '#fab82c',
+					width: 'fit-content',
+    			'flex-direction': 'row',
+    			'justify-content': 'center',
+					'align-items': 'center',
+					'border-radius': '1.5rem',
+					'font-weight': 'bold'
+        }
+      })
+    }
+	],
+}
